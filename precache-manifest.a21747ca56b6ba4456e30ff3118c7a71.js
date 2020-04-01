20492a9d11e1ff996219fa4aef2e0f3802cbc205
jags1891 <jagadeesh.bandlamudi@gmail.com>
2020-04-01 00:16:15 -0700

    Updates

---
+++ precache-manifest.a21747ca56b6ba4456e30ff3118c7a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180a934af2cf4c5d89ad6eb72691b37d",
+    "url": "/covid19Dashboard/index.html"
+  },
+  {
+    "revision": "3b7d5fb4d74f06c58b78",
+    "url": "/covid19Dashboard/static/css/main.1b596c42.chunk.css"
+  },
+  {
+    "revision": "14cf3c753954297a84c8",
+    "url": "/covid19Dashboard/static/js/2.ce1d8189.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19Dashboard/static/js/2.ce1d8189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7d5fb4d74f06c58b78",
+    "url": "/covid19Dashboard/static/js/main.bf613b57.chunk.js"
+  },
+  {
+    "revision": "cbcdf1fd53b641726b90",
+    "url": "/covid19Dashboard/static/js/runtime-main.c4a3fd13.js"
+  }
+]);
